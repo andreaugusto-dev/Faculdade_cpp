51f03ca403eb1957ed7459c5eae2722056b58f09
andreaugusto-dev <andre.augusto@aluno.unifenas.br>
2026-05-14 21:08:49 -0300

    Update media_idade.cpp

--- media_idade.cpp
+++ media_idade.cpp
@@ -5,14 +5,26 @@ int main() {
     int idade;
     int soma = 0;
     float media;
+    int total_pessoas = 0;
 
     for (int i = 1; i <= 10; i++) {
-        printf("Digite a idade da pessoa %d: ", i);
+        printf("Digite a idade da pessoa %d (ou 0 para sair): ", i);
         scanf("%d", &idade);
 
-        soma += idade;
-    }
-    media = soma / 10.0;
-    printf("\nA media das 10 idades e: %.2f anos\n", media);
+        if (idade == 0) {
+            break; 
+        }
 
+        soma += idade;
+        total_pessoas++;
+    }
+
+    if (total_pessoas > 0) {
+        media = (float)soma / total_pessoas;
+        printf("\nA media das %d idades e: %.2f anos\n", total_pessoas, media);
+    } else {
+        printf("\nNenhuma idade foi inserida.\n");
+    }
+
+    return 0;
 }
